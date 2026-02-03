c1490bb7b12de8cad3174682653047209f05663f
Nikita <nikita.mag.0027@gmail.com>
2026-02-03 18:17:45 +0100

    fix: resume file name

--- src/app/[locale]/components/navbar.tsx
+++ src/app/[locale]/components/navbar.tsx
@@ -71,10 +71,9 @@ export default function NavBar() {
                             {locale === "en" ? "FR" : "EN"}
                         </button>
                         <a
-                            href={locale === "en" ? "/eng/CV_Nikita_Magzoumov.pdf" : "/fr/CV_Nikita_Magzoumov.pdf"}
+                            href={locale === "en" ? "/eng/Resume_Nikita_Magzoumov.pdf" : "/fr/CV_Nikita_Magzoumov.pdf"}
                             target="_blank"
                             rel="noopener noreferrer"
-                            download="Resume_Nikita_Magzoumov.pdf"
                             className="px-2 py-0.5 border border-[var(--lightest-slate)] hover:border-[var(--green-bright)] text-[var(--lightest-slate)] rounded hover:text-[var(--green-bright)] hover:bg-opacity-10 transition-all duration-300 text-[13px] leading-tight"
                         >
                             Resume
